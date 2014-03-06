75176d5f610e87f3cdc38fe7b731cb431fb5936c
Ludwik Trammer <ludwik.trammer@nask.pl>
2014-03-06 12:36:47 +0100

    Treat second arguments ("title") to template tags as a variable

--- django_sortable/templatetags/sortable.py
+++ django_sortable/templatetags/sortable.py
@@ -10,8 +10,8 @@ SORT_DESC_CLASS = getattr(settings, 'SORT_DESC_CLASS' , 'sort-desc')
 SORT_NONE_CLASS = getattr(settings, 'SORT_DESC_CLASS' , 'sort-none')
 
 directions = {
-  'asc': {'class': SORT_ASC_CLASS, 'inverse': 'desc'}, 
-  'desc': {'class': SORT_DESC_CLASS, 'inverse': 'asc'}, 
+  'asc': {'class': SORT_ASC_CLASS, 'inverse': 'desc'},
+  'desc': {'class': SORT_DESC_CLASS, 'inverse': 'asc'},
 }
 
 
@@ -21,19 +21,19 @@ def parse_tag_token(token):
   if len(bits) < 2:
     raise TemplateSyntaxError, "anchor tag takes at least 1 argument"
   try:
-    title = bits[2]
+    title = template.Variable(bits[2])
   except IndexError:
     if bits[1].startswith(('+', '-')):
       title = bits[1][1:].capitalize()
     else:
       title = bits[1].capitalize()
-  
+
   return (bits[1].strip(), title.strip())
-  
+
 
 class SortableLinkNode(template.Node):
   """Build sortable link based on query params."""
-  
+
   def __init__(self, field_name, title):
     if field_name.startswith('-'):
       self.field_name = field_name[1:]
@@ -44,43 +44,43 @@ class SortableLinkNode(template.Node):
     else:
       self.field_name = field_name
       self.default_direction = 'asc'
-    
+
     self.title = title
-  
-  
+
+
   def build_link(self, context):
     """Prepare link for rendering based on context."""
     get_params = context['request'].GET.copy()
-    
+
     field_name = get_params.get('sort', None)
     if field_name:
       del(get_params['sort'])
-    
+
     direction = get_params.get('dir', None)
     if direction:
       del(get_params['dir'])
     direction = direction if direction in ('asc', 'desc') else 'asc'
-      
+
     # if is current field, and sort isn't defined, assume asc otherwise desc
     direction = direction or ((self.field_name == field_name) and 'asc' or 'desc')
-    
+
     # if current field and it's sorted, make link inverse, otherwise defalut to asc
     if self.field_name == field_name:
       get_params['dir'] = directions[direction]['inverse']
     else:
       get_params['dir'] = self.default_direction
-    
+
     if self.field_name == field_name:
       css_class = directions[direction]['class']
     else:
       css_class = SORT_NONE_CLASS
-    
+
     params = "&%s" % (get_params.urlencode(),) if len(get_params.keys()) > 0 else ''
     url = ('%s?sort=%s%s' % (context['request'].path, self.field_name, params)).replace('&', '&amp;')
-    
+
     return (url, css_class)
-    
-  
+
+
   def render(self, context):
     url, css_class = self.build_link(context)
     return '<a href="%s" class="%s" title="%s">%s</a>' % (url, css_class, self.title, self.title)
@@ -88,7 +88,7 @@ class SortableLinkNode(template.Node):
 
 class SortableTableHeaderNode(SortableLinkNode):
   """Build sortable link header based on query params."""
-  
+
   def render(self, context):
     url, css_class = self.build_link(context)
     return '<th class="%s"><a href="%s" title="%s">%s</a></th>' % (css_class, url, self.title, self.title)
@@ -96,7 +96,7 @@ class SortableTableHeaderNode(SortableLinkNode):
 
 class SortableURLNode(SortableLinkNode):
   """Build sortable link header based on query params."""
-  
+
   def render(self, context):
     url, css_class = self.build_link(context)
     return url
@@ -104,7 +104,7 @@ class SortableURLNode(SortableLinkNode):
 
 class SortableClassNode(SortableLinkNode):
   """Build sortable link header based on query params."""
-  
+
   def render(self, context):
     url, css_class = self.build_link(context)
     return css_class
@@ -129,7 +129,7 @@ def sortable_class(parser, token):
   field, title = parse_tag_token(token)
   return SortableClassNode(field, title)
 
-  
+
 sortable_link = register.tag(sortable_link)
 sortable_header = register.tag(sortable_header)
 sortable_url = register.tag(sortable_url)
